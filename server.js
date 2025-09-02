c9ed554b3d263b2f39e56755a902fedeb41abe0a
alaalshehabii <alaa.hsan.hajj@gmail.com>
2025-09-02 22:30:31 +0300

    gretings

--- server.js
+++ server.js
@@ -1,2 +1,9 @@
 const express = require('express');
 const app = express();
+// Loading Express
+
+// ======================== 1. Greetings ========================
+app.get('/greetings/:userName', (req, res) => {
+  const { userName } = req.params;
+  res.send(`Hello there, ${userName}!`);
+});
